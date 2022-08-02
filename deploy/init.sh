1954180bbe7048899a4b5d70a76439c7a540e5e2
Tom Hart <thomas.hart@sky.uk>
2022-08-02 10:40:31 +0100

    Tag

--- deploy/init.sh
+++ deploy/init.sh
@@ -10,6 +10,11 @@ if [ -n "$image_tag" ]; then
   sed -i.bak "s/\$TAG/$image_tag/" ./values.yaml
 fi
 
+if echo "$image_tag" | grep -Eq '[0-9]+.[0-9]+.[0-9]+'; then
+   echo 'Deploying a tag'
+   subdomain='anxiety'
+fi
+
 if [ -n "$subdomain" ]; then
   echo 'Swap host';
   sed -i.bak "s/\$HOST/$subdomain.tom-hart.net/" ./values.yaml
