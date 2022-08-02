b66ed361b92e4eeba1d3af40feef21f7d65c9b5a
Tom Hart <thomas.hart@sky.uk>
2022-08-02 09:44:47 +0100

    init script

--- deploy/init.sh
+++ deploy/init.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env sh
 
-echo "Image-Name: " "$image_name"
+if [ ! -f "values.yaml" ]; then
+  echo 'No values.yaml file to process';
+  exit 0;
+fi
+
+if [ -n "$image_tag" ]; then
+  echo 'Swap image tag';
+  sed -i.bak "s/\$TAG/$image_tag/" ./values.yaml
+fi
+
+if [ -n "$subdomain" ]; then
+  echo 'Swap host';
+  sed -i.bak "s/\$HOST/$subdomain.tom-hart.net/" ./values.yaml
+fi
